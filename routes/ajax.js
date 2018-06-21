fc7534a129941c55d70932004a9f6604f069a74b
ParagDineshGupta <guptaparag1996@gmail.com>
2018-06-21 12:35:13 +0530

    Update ajax.js

--- routes/ajax.js
+++ routes/ajax.js
@@ -3,6 +3,8 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 var url=require('url');
+
+
 var mysql = require('mysql');
 var dbcon = require('./dbcon');
 var con = mysql.createConnection(dbcon.con);
