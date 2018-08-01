2c017f5d89904312c7a4d39511784e410cee203c
ParagDineshGupta <guptaparag1996@gmail.com>
2018-08-01 19:22:20 +0530

    Update dbcon.js

--- routes/dbcon.js
+++ routes/dbcon.js
@@ -1,10 +1,19 @@
 
 module.exports ={
 
- con : {
-    host: "mydbinstance.cq4rkzpvmufw.ap-south-1.rds.amazonaws.com",
-    user: "parag",
-    password: "equipshare",
+//  con : {
+//     host: "mydbinstance.cq4rkzpvmufw.ap-south-1.rds.amazonaws.com",
+//     user: "parag",
+//     password: "equipshare",
+//     database: "equip",
+//   multipleStatements: true
+
+// }
+ 
+  con : {
+    host: "equip.cnv3bnhwzpw1.us-east-2.rds.amazonaws.com",
+    user: "chitransh",
+    password: "mission1",
     database: "equip",
   multipleStatements: true
 
